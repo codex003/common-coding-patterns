c4715a56e66d4c4fd36a9154b9aa65f5d4f340d4
codex003 <35473982+codex003@users.noreply.github.com>
2022-06-04 08:48:23 -0400

    Leetcode - Easy(925) - Long Pressed Name

---
+++ src/io/dev/codex/LongPressedNameProblem.java
@@ -0,0 +1,30 @@
+package io.dev.codex;
+
+// Reference online problems
+// Leetcode: 925. Long Pressed Name
+// https://leetcode.com/problems/long-pressed-name/
+public class LongPressedNameProblem {
+    public boolean isLongPressedName(String name, String typed) {
+        if(name.length()>typed.length()) return false;
+        int i=0;
+        int j = 0;
+        while(i<name.length() && j<typed.length()){
+            if(name.charAt(i) == typed.charAt(j)){
+                i++;
+                j++;
+            }
+            else if(i>0 && name.charAt(i-1) == typed.charAt(j)){
+                j++;
+            }
+            else{
+                return false;
+            }
+        }
+
+        while(j<typed.length()){
+            if(name.charAt(i-1) != typed.charAt(j)) return false;
+            j++;
+        }
+        return i<name.length() ? false : true;
+    }
+}
